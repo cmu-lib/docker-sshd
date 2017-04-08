a884ce1e69fccbb1ac5989322f8338275848b1ec
Brian S. Woolstrum <bsw2@andrew.cmu.edu>
2017-04-08 08:51:59 -0400

    we need a user its home directory to login with

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,10 @@
 
 /usr/bin/ssh-keygen -A
 
+
+groupadd -g ${SSHD_GROUP_ID:=500} ${SSHD_GROUP_NAME:=guest}
+useradd -g ${SSHD_GROUP_ID:=500} -u  ${SSHD_USER_ID:=500} -d /home/${SSHD_USER_NAME:=guest} ${SSHD_USER_NAME:=guest}
+chown ${SSHD_USER_NAME:=guest}:${SSHD_GROUP_NAME:=guest} /home/${SSHD_USER_NAME:=guest}
+chown -R ${SSHD_USER_NAME:=guest} /home/${SSHD_USER_NAME:=guest}/.ssh
+
 exec "$@"
